02de76c946ac02c5855d73a4df7bbcb01ee903b3
vastopol <classicunix@gmail.com>
2017-03-31 11:42:02 +0000

    lisp code && notes

---
+++ lsp/harness.lisp
@@ -0,0 +1,59 @@
+;;; test harness
+
+(load "simple.lisp")
+
+#|
+
+; test triangular
+(print "test triangular")
+(print (triangular 1))
+(print (triangular 10))
+(print (triangular 100))
+
+; test factorial
+(print "test factorial")
+(print (fac1 5))
+(print (fac1 10))
+(print (/ (fac1 100) (fac1 99)))
+
+; test fibonacci 1
+(print "test fib1")
+(print (fib1 0))
+(print (fib1 1))
+(print (fib1 5))
+(print (fib1 10))
+
+; test fibonacci 2
+(print "test fib2")
+(print (fib2 0))
+(print (fib2 1))
+(print (fib2 5))
+(print (fib2 10))
+
+; test lucas 1
+(print "test lucas1")
+(print (lucas1 0))
+(print (lucas1 1))
+(print (lucas1 5))
+(print (lucas1 10))
+
+; test lucas 2
+(print "test lucas2")
+(print (lucas2 0))
+(print (lucas2 1))
+(print (lucas2 5))
+(print (lucas2 10))
+
+; test binomial coefficient
+(print "test binomial")
+(print (binomial 4 2))
+(print (binomial 6 3))
+(print (binomial 8 4))
+(print (binomial 5 0))
+(print (binomial 5 1))
+(print (binomial 5 2))
+(print (binomial 5 3))
+(print (binomial 5 4))
+(print (binomial 5 5))
+
+|#
--- lsp/simple.lisp
+++ lsp/simple.lisp
@@ -1,31 +1,64 @@
 ;;; Common Lisp
-;;;
 ;;; Testing some simple recursive functions
 ;;; probably not the best designs for runtime...
+;;;---------------------------------------------
 
-;; factorial function
-(defun fac1 (x)
-    (if (<= x 1)
+;; triangular numbers
+(defun triangular (n)
+    (if (<= n 1)
         1
-      (* x (fac1 (- x 1)))))
+      (+ n (triangular (- n 1)))))
 ;---------------------------
 
+;; factorial function
+(defun fac1 (n)
+    (if (<= n 1)
+        1
+      (* n (fac1 (- n 1)))))
+;---------------------------
 
 ;; fibonacci numbers 1
-(defun fib1 (x)
-    (if (or (zerop x) (= x 1))
+(defun fib1 (n)
+    (if (or (zerop n) (= n 1))
         1
-      (+ (fib1 (- x 1)) (fib1 (- x 2)))))
+      (+ (fib1 (- n 1)) (fib1 (- n 2)))))
 ;----------------------------
 
-
 ;; fibonacci numbers 2
-(defun fib2 (x)
-    (if (or (zerop x) (= x 1))
+(defun fib2 (n)
+    (if (or (zerop n) (= n 1))
         1
       (let 
-        ((f1 (fib2 (- x 1)))
-         (f2 (fib2 (- x 2))))
-      (+ f1 f2))))
+          ((f1 (fib2 (- n 1)))
+           (f2 (fib2 (- n 2))))
+        (+ f1 f2))))
 ;-------------------------------
+
+;; lucas numbers 1
+(defun lucas1 (n)
+    (cond ((= n 0) 2)
+          ((= n 1) 1)
+          (t (+ (lucas1 (- n 1)) (lucas1 (- n 2))))))
+;--------------------------------------------------
+
+;; lucas numbers 2
+(defun lucas2 (n)
+    (cond ((= n 0) 2)
+          ((= n 1) 1)
+          (t 
+            (let
+                ((l1 (lucas2 (- n 1)))
+                 (l2 (lucas2 (- n 2))))
+              (+ l1 l2)))))
+;--------------------------------------------------
+
+;; binomial coefficient
+(defun binomial (n r)
+    (if (or (= r 0) (= r n))
+        1
+      (let
+          ((b1 (binomial (- n 1) (- r 1)))
+           (b2 (binomial (- n 1) r)))
+        (+ b1 b2))))
+;----------------------------------------------------
 
