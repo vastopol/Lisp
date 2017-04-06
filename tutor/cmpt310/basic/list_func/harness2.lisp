021f7c82105dcd2df5f20c972c16ec29d389e476
vastopol <classicunix@gmail.com>
2017-04-06 10:21:58 +0000

    lisp tutorial #1 cs.sfu.ca

--- tutor/cmpt310/basic/list_func/harness2.lisp
+++ tutor/cmpt310/basic/list_func/harness2.lisp
@@ -2,15 +2,14 @@
 
 (load "structural.lisp")
 
-#|
-
 ; test recursive list length
 (print "list-length")
 (print (recur-list-length `nil))
 (print (recur-list-length `(1)))
-(print (recur-list-length '(1 2 3 4)))
+(print (recur-list-length `(1 2 3 4)))
 (print (recur-list-length `(1 2 3 4 5 6 7 8 9 10)))
 
+; test nth v1
 (print "list-nth v1")
 (print (list-nth-v1 5 `nil))
 (print (list-nth-v2 0 `nil))
@@ -20,6 +19,7 @@
 (print (list-nth-v1 20 `(0 1 2 3 4 5 6 7 8 9)))
 (print (list-nth-v1 -1 `(0 1 2 3 4 5 6 7 8 9)))
 
+; test nth v2
 (print "list-nth v2")
 (print (list-nth-v2 5 `nil))
 (print (list-nth-v2 0 `nil))
@@ -29,6 +29,7 @@
 (print (list-nth-v2 20 `(0 1 2 3 4 5 6 7 8 9)))
 (print (list-nth-v2 -1 `(0 1 2 3 4 5 6 7 8 9)))
 
+; test last
 (print "last")
 (print (list-last `nil))
 (print (list-last `(1)))
@@ -36,5 +37,25 @@
 (print (list-last `(1 2 a b 3 c)))
 (print (list-last `(q w e r a s d)))
 
-|#
+; test member
+(print (list-member 1 `nil))
+(print (list-member 1 `(1)))
+(print (list-member 2 `(1 1 1 1)))
+(print (list-member 3 `(1 1 1 1 1 1 3 2 2 2)))
 
+; test append
+(print (list-append `nil `nil))
+(print (list-append `nil `(1 2)))
+(print (list-append `(1 2 3) `(4 5 6)))
+(print (list-append `(1 2 4) `nil))
+
+;test butlast
+(print (list-butlast `nil))
+(print (list-butlast `(1)))
+(print (list-butlast `(1 2)))
+(print (list-butlast `(9 8 7 6 5 4 3 2 1 0)))
+
+; test intersection
+(print (list-intersection `(1 3 5 7) `(1 2 3 4)))
+(print (list-intersection `(1 2 3 4) `(5 6 7 8)))
+(print (list-intersection `nil `(1 2 3)))
--- tutor/cmpt310/basic/list_func/structural.lisp
+++ tutor/cmpt310/basic/list_func/structural.lisp
@@ -1,4 +1,5 @@
 ;;; Functions for list manipulation
+;;; linear recursion
 ;----------------------------------
 
 ;; Recursive List Length
@@ -28,8 +29,38 @@
 ;; last item in list
 (defun list-last (L)
     (cond
-      ((null L) nil)
-      ((null    (rest L)) (first L))
-      (t        (list-last (rest L)))))
+      ((null L) 		nil)
+      ((null (rest L))  (first L))
+      (t        		(list-last (rest L)))))
 ;----------------------------------------------
 
+;; true if e is in list L
+(defun list-member (e L)
+	(cond
+		((null L)			nil)
+		 ((eq e (first L))	t)
+		 (t					(list-member e (rest L)))))
+;----------------------------------------------
+
+;; append list L2 to List L1
+(defun list-append (L1 L2)
+	(if (null L1)
+		L2
+	  (cons (first L1) (list-append (rest L1) L2))))
+;---------------------------------------------------
+
+;; return all but last in list
+(defun list-butlast (L)
+	(if (consp (rest L))
+		(cons (first L) (list-butlast (rest L)))
+	  nil))
+;---------------------------------------------------
+
+;; intersection of sets L1 && L2
+(defun list-intersection (L1 L2)
+	(cond
+		((null L1) nil)
+		((member (first L1) L2)
+			(cons (first L1) (list-intersection (rest L1) L2)))
+		(t  (list-intersection (rest L1) L2))))
+;-------------------------------------------------------
